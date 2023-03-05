45c1c24a3d7730bb450f62f1fab885e22d6c21e1
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2023-03-05 12:14:55 +0100

    Deleted unused Input component

--- src/components/Input.tsx
+++
@@ -1,6 +0,0 @@
-import { Input as CInput, InputProps, forwardRef } from '@chakra-ui/react';
-
-const Input = forwardRef(({ ...rest }: InputProps, ref) => {
-  return <CInput ref={ref} focusBorderColor={'blue.800'} {...rest} />;
-});
-export default Input;
